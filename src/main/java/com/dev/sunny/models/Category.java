6a3507167a5661b97621da629a99c42ddd17a608
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-02-04 11:39:12 +0530

    Add flyway core and mysql to pom.xml for flyway migration. Add attribute description to Category. Add h2 and mysql flyway init migration scripts. Add h2 and mysql flyway versioned migration script. Modify properties in application.properties  - change ddl-auto from update to validate.  - change url from mem to tcp.  - add flyway locations based on profile.  - add config 'spring.profiles.active' to activate h2 profile.

--- src/main/java/com/dev/sunny/models/Category.java
+++ src/main/java/com/dev/sunny/models/Category.java
@@ -1,6 +1,7 @@
 package com.dev.sunny.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -13,6 +14,8 @@ import java.util.List;
 @Entity
 public class Category extends BaseModel {
     private String name;
+    @Column(length = 500)
+    private String description;
 //    Describe the relationship between Category and Product
 //    One category can have many products
     @JsonIgnore
---
+++ src/main/resources/db/migration/h2/V2__add-description-to-category.sql
@@ -0,0 +1,2 @@
+ALTER TABLE category
+    ADD description VARCHAR(500);
