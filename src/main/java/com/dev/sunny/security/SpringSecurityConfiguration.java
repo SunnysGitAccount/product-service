3c9eea1a0a197ed3f6ee0a0c2d67fcc38b354986
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-03-09 11:17:52 +0530

    Add changes to implement OAuth2 spring security to all endpoints using auth server.

--- src/main/java/com/dev/sunny/security/SpringSecurityConfiguration.java
+++ src/main/java/com/dev/sunny/security/SpringSecurityConfiguration.java
@@ -12,6 +12,13 @@ public class SpringSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/products/create").hasAuthority("SCOPE_ADMIN")
+                        .requestMatchers("/api/products/all",
+                                "/api/products/create",
+                                "/api/products/delete/{id}",
+                                "/api/products/replace/{id}",
+                                "/api/products/update/{id}").authenticated()
+                        .requestMatchers("/api/products/category/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2
